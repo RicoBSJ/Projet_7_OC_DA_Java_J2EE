cd68d7eebc6b2501e72158d6d9dbe94ea4265c83
RicoBSJ <44947358+RicoBSJ@users.noreply.github.com>
2020-09-13 18:08:54 +0200

    AnnotationsSpringJpaRepositoryEtc

---
+++ library-consumer/src/main/java/com/aubrun/eric/projet7/consumer/repository/AuthorRepository.java
@@ -0,0 +1,11 @@
+package com.aubrun.eric.projet7.consumer.repository;
+
+import com.aubrun.eric.projet7.beans.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Integer> {
+
+
+}
