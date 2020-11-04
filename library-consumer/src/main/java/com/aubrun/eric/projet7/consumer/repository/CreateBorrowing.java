a9413cac9f51343dff0e37fe27d5ee6c32da22ad
RicoBSJ <44947358+RicoBSJ@users.noreply.github.com>
2020-11-04 23:40:07 +0100

    RepositoryBorrowingMethod

--- library-consumer/src/main/java/com/aubrun/eric/projet7/consumer/repository/CreateBorrowing.java
+++ library-consumer/src/main/java/com/aubrun/eric/projet7/consumer/repository/CreateBorrowing.java
@@ -3,6 +3,7 @@ package com.aubrun.eric.projet7.consumer.repository;
 import com.aubrun.eric.projet7.beans.Book;
 import com.aubrun.eric.projet7.beans.Borrowing;
 import com.aubrun.eric.projet7.beans.UserAccount;
+import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -15,6 +16,11 @@ public class CreateBorrowing {
     private EntityManager entityManager;
 
     public Borrowing createBorrowing(UserAccount userAccount, Book book){
+        Session session = entityManager.unwrap(Session.class);
+        userAccount = (UserAccount) session.getEntityManagerFactory();
+        if (userAccount == null){
+
+        }
         Borrowing borrowing = new Borrowing();
         borrowing.getBorrowingId();
         book = borrowing.getBookBorrowing();
