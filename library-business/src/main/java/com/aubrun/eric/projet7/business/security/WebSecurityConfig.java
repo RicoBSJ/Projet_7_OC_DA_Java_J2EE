7495766e80ce070563cf48109b081c19409157c8
RicoBSJ <44947358+RicoBSJ@users.noreply.github.com>
2020-12-24 14:11:12 +0100

    CreateBorrowingOK

--- library-business/src/main/java/com/aubrun/eric/projet7/business/security/WebSecurityConfig.java
+++ library-business/src/main/java/com/aubrun/eric/projet7/business/security/WebSecurityConfig.java
@@ -63,6 +63,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
                 .antMatchers(HttpMethod.GET, "/books/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/authors/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/users/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/users/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/books/search/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/borrowings/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/borrowings/**").permitAll()
