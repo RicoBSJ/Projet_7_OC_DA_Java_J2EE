e0684f967919bfde46aab3c7109d5285f87ec53c
RicoBSJ <44947358+RicoBSJ@users.noreply.github.com>
2021-01-28 00:23:53 +0100

    AuthControllerAuthTokenFilterRoleRepositoryModifications

--- library-consumer/src/main/java/com/aubrun/eric/projet7/consumer/repository/RoleRepository.java
+++ library-consumer/src/main/java/com/aubrun/eric/projet7/consumer/repository/RoleRepository.java
@@ -3,9 +3,11 @@ package com.aubrun.eric.projet7.consumer.repository;
 import com.aubrun.eric.projet7.beans.ERole;
 import com.aubrun.eric.projet7.beans.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 
     Optional<Role> findByRoleName(ERole roleName);
