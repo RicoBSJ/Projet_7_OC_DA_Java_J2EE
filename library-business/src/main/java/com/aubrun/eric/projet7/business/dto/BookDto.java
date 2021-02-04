5bdb3b68751e082b23de25d9878dd511044b615f
RicoBSJ <44947358+RicoBSJ@users.noreply.github.com>
2021-02-04 23:16:14 +0100

    ModificationsBookDtoDataSqlWebSecurityConfig

--- library-business/src/main/java/com/aubrun/eric/projet7/business/dto/BookDto.java
+++ library-business/src/main/java/com/aubrun/eric/projet7/business/dto/BookDto.java
@@ -1,7 +1,5 @@
 package com.aubrun.eric.projet7.business.dto;
 
-import java.util.Date;
-
 public class BookDto {
 
     private Integer bookId;
--- library-business/src/main/java/com/aubrun/eric/projet7/business/security/WebSecurityConfig.java
+++ library-business/src/main/java/com/aubrun/eric/projet7/business/security/WebSecurityConfig.java
@@ -63,6 +63,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
                 .antMatchers(HttpMethod.GET, "/books/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/authors/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/books/search/**").permitAll()
+                .antMatchers("/borrowings/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
                 .anyRequest().authenticated();
 
